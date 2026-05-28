90fdf85613c951842b3e7f5295dffe92814ae3f0
Ashi210803 <ashisingh2103@outlook.com>
2026-05-28 15:45:05 +0530

    p

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,42 +1,29 @@
-import { useEffect, useRef } from "react";
-
 import "../styles/loader.css";
 
 function PageLoader() {
-
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-
-    if (videoRef.current) {
-
-      videoRef.current.play()
-        .catch(() => {});
-    }
-
-  }, []);
 
   return (
 
     <div className="page-loader">
 
       <video
-        ref={videoRef}
-        muted
-        playsInline
-        webkit-playsinline="true"
-        preload="auto"
-        className="loader-video"
-      >
+  autoPlay
+  muted
+  loop
+  playsInline
+  webkit-playsinline="true"
+  preload="auto"
+  className="loader-video"
+>
 
-        <source
-          src="/loader.mp4"
-          type="video/mp4"
-        />
+  <source
+    src="/loader (2).mp4"
+    type="video/mp4"
+  />
 
-      </video>
-
+</video>
     </div>
+
   );
 }
 
