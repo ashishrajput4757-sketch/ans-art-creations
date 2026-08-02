e01255e3b0743a8dc62ef12ab4f9d4791c6580e6
Ashi210803 <ashisingh2103@outlook.com>
2026-08-02 19:10:25 +0530

    quick update

---
+++ frontend/src/components/PageLoader/PageLoader.jsx
@@ -0,0 +1,19 @@
+import "./loader.css";
+
+function PageLoader() {
+
+  return (
+
+    <div className="page-loader">
+
+ <img
+  src={`/Loader.svg?v=${Date.now()}`}
+  alt="Loading..."
+  className="loader-svg"
+/>
+    </div>
+
+  );
+}
+
+export default PageLoader;
