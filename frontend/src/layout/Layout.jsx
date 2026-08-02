cd0bdd2dca27e0f8db2932b3eab5855d64859076
Ashi210803 <ashisingh2103@outlook.com>
2026-08-02 19:25:16 +0530

    quick update

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,10 +1,7 @@
-import Navbar from "../components/Navbar";
 
 function Layout({ children }) {
   return (
     <>
-      {/* GLOBAL NAVBAR */}
-      <Navbar />
 
       {/* PAGE CONTENT */}
       <main className="main-content">
