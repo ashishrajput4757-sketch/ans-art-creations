79342d580ac53edfc6258851ec539bcc523b99e0
Ashish Singh <ashishrajput4757@gmail.com>
2026-08-20 18:50:26 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import EcoSection from "./components/EcoSection/EcoSection";
 import Customization from "./components/Customization/Customization";
 import SignatureCollections from "./components/SignatureCollections/SignatureCollections";
 import Gallery from "./components/Gallery/Gallery";
-/*import Testimonials from "./components/Testimonials/Testimonials";*/
+import Testimonials from "./components/Testimonials/Testimonials";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
